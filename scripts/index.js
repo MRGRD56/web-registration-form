9fe7bd19f90ffc5c3be114c77d394efc8c9995c2
MRGRD56 <kirgol85@gmail.com>
2021-09-24 20:12:01 +0500

    add IE11 support

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const formCloseButtons = document.querySelectorAll(".form-close-button");
+const formCloseButtons = document.querySelectorAll("button.form-close-button");
 const registrationFormsChooseButtons = document.querySelectorAll(
     ".registration-wrapper > .registration-forms-choose > div > button");
 const registrationFormsWrappers = document.querySelectorAll(".registration-form-wrapper");
@@ -13,33 +13,43 @@ function setVisible(element, isVisible) {
     }
 }
 
-registrationFormsChooseButtons.forEach((button, buttonIndex) => {
-    button.addEventListener("click", () => {
+for (let buttonIndex = 0; buttonIndex < registrationFormsChooseButtons.length; buttonIndex++) {
+    const button = registrationFormsChooseButtons[buttonIndex];
+    const buttonFormId = button.getAttribute("data-form");
+    button.addEventListener("click", function () {
         setVisible(registrationFormsChoose, false);
-        registrationFormsWrappers.forEach((formWrapper, formWrapperIndex) => {
-            setVisible(formWrapper, formWrapperIndex === buttonIndex);
-        });
+        for (let formWrapperIndex = 0; formWrapperIndex < registrationFormsWrappers.length; formWrapperIndex++) {
+            const formWrapper = registrationFormsWrappers[formWrapperIndex];
+            const formId = formWrapper.getAttribute("data-form");
+            setVisible(formWrapper, formId === buttonFormId);
+        }
     });
-});
+}
 
-formCloseButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        registrationFormsWrappers.forEach(formWrapper => {
-            setVisible(formWrapper, false);
-        });
+for (let i = 0; i < formCloseButtons.length; i++) {
+    const button = formCloseButtons[i];
+    const buttonFormId = button.getAttribute("data-form");
+    button.addEventListener("click", function () {
+        const formWrapper = Array.from(registrationFormsWrappers)
+            .filter(function (fw) {
+                return fw.getAttribute("data-form") === buttonFormId;
+            })[0];
+        setVisible(formWrapper, false);
         setVisible(registrationFormsChoose, true);
     });
-});
+}
 
-registrationForms.forEach(form => {
-    form.addEventListener("submit", event => {
+for (let i = 0; i < registrationForms.length; i++) {
+    const form = registrationForms[i];
+    form.addEventListener("submit", function (event) {
         event.preventDefault();
         const formInputs = Array.from(event.target.querySelectorAll("input"));
-        const formData = formInputs.reduce((data, input) => {
+        const formData = formInputs.reduce(function (data, input) {
             const key = input.name;
             const value = input.type === "checkbox" ? input.checked : input.value;
-            return {[key]: value, ...data};
+            data[key] = value;
+            return data;
         }, {});
         console.log(formData);
     });
-});
+}
