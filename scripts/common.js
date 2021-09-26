e5aa08db0ee4a6c6260fb31749043c3f9cd533ee
MRGRD56 <kirgol85@gmail.com>
2021-09-26 18:53:25 +0500

    add JS forms validation

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,15 @@
+function removeHtmlElement(element) {
+    if (element.remove) {
+        element.remove();
+    } else {
+        element.parentNode.removeChild(element);
+    }
+}
+
+function insertAfterElement(element, newElement) {
+    element.parentNode.insertBefore(newElement, element.nextSibling);
+}
+
 function arrayFrom(value) {
     if (Array.from) {
         return Array.from(value);
--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,57 @@ const registrationFormsContainers = document.querySelectorAll(".registration-for
 const registrationFormsChoose = document.querySelector(".registration-forms-choose");
 const registrationForms = document.querySelectorAll("form.registration-form");
 
+function setFieldValidationMessage(field, message) {
+    const nextSibling = field.nextSibling;
+    const validationDiv = nextSibling && nextSibling.classList
+        ? (nextSibling.classList.contains("validation-message") ? nextSibling : null)
+        : null;
+
+    if (validationDiv) {
+        if (!message) {
+            removeHtmlElement(validationDiv);
+        } else {
+            validationDiv.innerText = message;
+        }
+    } else if (message) {
+        const newValidationDiv = document.createElement("div");
+        newValidationDiv.classList.add("validation-message");
+        newValidationDiv.innerText = message;
+        insertAfterElement(field, newValidationDiv);
+    }
+}
+
+function validateField(field) {
+    const validity = field.validity;
+    const validationMessage = field.getAttribute("data-validation-msg");
+
+    const isValid = validity.valid;
+    const isEmpty = validity.valueMissing;
+
+    const message = isValid
+        ? null
+        : isEmpty
+            ? "Заполните это поле"
+            : validationMessage;
+
+    setFieldValidationMessage(field, message);
+    return isValid;
+}
+
+function validateForm(form) {
+    let isValid = true;
+
+    const inputs = form.querySelectorAll("input");
+    for (let i = 0; i < inputs.length; i++) {
+        const isFieldValid = validateField(inputs[i]);
+        if (!isFieldValid && isValid) {
+            isValid = false;
+        }
+    }
+
+    return isValid;
+}
+
 function setVisible(element, isVisible) {
     if (isVisible) {
         element.classList.remove("invisible");
@@ -41,9 +92,20 @@ for (let i = 0; i < formCloseButtons.length; i++) {
 
 for (let i = 0; i < registrationForms.length; i++) {
     const form = registrationForms[i];
+    const formInputs = arrayFrom(form.querySelectorAll("input"));
+
+    for (let j = 0; j < formInputs.length; j++) {
+        const formInput = formInputs[j];
+        formInput.addEventListener("input", function () {
+            setFieldValidationMessage(formInput, null);
+        });
+    }
+
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-        const formInputs = arrayFrom(event.target.querySelectorAll("input"));
+        const isFormValid = validateForm(form);
+        if (!isFormValid) return;
+
         const formData = formInputs.reduce(function (data, input) {
             const key = input.name;
             const value = input.type === "checkbox" ? input.checked : input.value;
